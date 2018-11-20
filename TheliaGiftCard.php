c060157d8eff3d834ec2ae765f76eb519b54ba72
zawaze <btourlonias@openstudio.fr>
2018-11-20 22:07:56 +0100

    init commit

---
+++ TheliaGiftCard.php
@@ -0,0 +1,16 @@
+<?php
+/*************************************************************************************/
+/*      Copyright (c) BERTRAN TOULONIAS                                              */
+/*      email : btourlonias@openstudio.fr                                            */
+/*************************************************************************************/
+
+namespace TheliaGiftCard;
+
+use Thelia\Module\BaseModule;
+
+class TheliaGiftCard extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'theliagiftcard';
+
+}
